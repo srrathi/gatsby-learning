2096febcf27c22b8897dcd8d697aa76d0a4d2c40
srrathi <srrathi2000@gmail.com>
2021-02-26 18:09:16 +0530

    setting page for body data from contentful cms

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,44 +1,76 @@
 const path = require('path');
-module.exports.onCreateNode = ({ node, actions }) => {
-    const { createNodeField } = actions;
-    // console.log(node);
-    if (node.internal.type === "MarkdownRemark") {
-        const slug = path.basename(node.fileAbsolutePath, '.md')
-        createNodeField({
-            node,
-            name: 'slug',
-            value: slug
-        })
-    }
-}
+
+// Used for Markdown Pages to create slugs
+// module.exports.onCreateNode = ({ node, actions }) => {
+//     const { createNodeField } = actions;
+//     // console.log(node);
+//     if (node.internal.type === "MarkdownRemark") {
+//         const slug = path.basename(node.fileAbsolutePath, '.md')
+//         createNodeField({
+//             node,
+//             name: 'slug',
+//             value: slug
+//         })
+//     }
+// }
+
+// module.exports.createPages = async ({ graphql, actions }) => {
+//     const { createPage } = actions
+//     const blogTemplate = path.resolve('./src/templates/Blog.js')
+//     const res = await graphql(`
+//     query{
+//         allMarkdownRemark{
+//           edges{
+//             node{
+//               fields{
+//                 slug
+//               }
+
+//             }
+//           }
+//         }
+//       }
+//     `)
+//     res.data.allMarkdownRemark.edges.forEach((edge)=>{
+//         createPage({
+//             component: blogTemplate,
+//             path: `/blog/${edge.node.fields.slug}`,
+//             context:{
+//                 slug: edge.node.fields.slug
+//             }
+//         })
+//     })
+//     // 1. Get path to template
+//     // 2. Get markdown data
+//     // 3. Create new Pages
+// }
+
+
+
+// here we will fetch the body data from contentful CMS
 
 module.exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
-    const blogTemplate = path.resolve('./src/templates/Blog.js')
-    const res = await graphql(`
+    const blogTemplate=path.resolve('./src/templates/Blog.js')
+    const res=await graphql(`
     query{
-        allMarkdownRemark{
-          edges{
-            node{
-              fields{
-                slug
-              }
-              
+        allContentfulBlogPost{
+            edges{
+                node{
+                    slug
+                }
             }
-          }
         }
-      }
+    }
     `)
-    res.data.allMarkdownRemark.edges.forEach((edge)=>{
+
+    res.data.allContentfulBlogPost.edges.forEach((edge)=>{
         createPage({
             component: blogTemplate,
-            path: `/blog/${edge.node.fields.slug}`,
-            context:{
-                slug: edge.node.fields.slug
+            path: `/blog/${edge.node.slug}`,
+            context: {
+                slug: edge.node.slug
             }
         })
     })
-    // 1. Get path to template
-    // 2. Get markdown data
-    // 3. Create new Pages
 }
--- src/templates/Blog.js
+++ src/templates/Blog.js
@@ -3,34 +3,45 @@ import React from 'react'
 import Layout from '../components/layout';
 import postStyles from '../styles/post.module.scss'
 
-export const query = graphql`
-query(
-    $slug: String!
-  ){
-    markdownRemark(
-      fields: {
-        slug:{
-          eq:$slug
-        }
-      }
-    ){
-      frontmatter{
-        title
-        date
-      }
-      html
-    }
-  }
-`
+// for getting of Markdown Blogs
+// export const query = graphql`
+// query(
+//     $slug: String!
+//   ){
+//     markdownRemark(
+//       fields: {
+//         slug:{
+//           eq:$slug
+//         }
+//       }
+//     ){
+//       frontmatter{
+//         title
+//         date
+//       }
+//       html
+//     }
+//   }
+// `
+
+// for fetching body data to new pages from contentful 
+
+
+
 const Blog = (props) => {
     return (
         <Layout>
+
+          {/* For Markdown Posts
             <div className={postStyles.post}>
                 <h1>{props.data.markdownRemark.frontmatter.title}</h1>
                 <p>Published on: {props.data.markdownRemark.frontmatter.date}</p>
                 <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}>
                 </div>
-            </div>
+            </div> */}
+
+
+            
         </Layout>
     )
 }
